c59559a6f98ee6a2a82a93cf76917d29dcf0c77f
Lukasz Raczylo <lukasz@raczylo.com>
2015-08-23 15:55:00 +0100

    Working sample plugins displaying information about organisation members and listing organisations.

--- plugins/list_users.rb
+++ plugins/list_users.rb
@@ -16,7 +16,22 @@ module Plugins
 
     def self.run
       # This is the main method executed on plugin run
-      puts "running listusers here"
+      $users_data = Array.new
+      i         = 1
+      max_width = 0
+      $connection.organizations.each do |org|
+        begin
+          $connection.organization_members(org.login, :per_page => 200).each do |m|
+            m.html_url.length <= max_width ? max_width : max_width = m.html_url.length
+            $users_data.push(:id => i, :organization => org.login, :login => m.login, :github_id => m.id, :profile_url => m.html_url)
+            i += 1
+          end
+        rescue Octokit::NotFound
+          Printer.print("warn", "Can't list members for this organisation. Insufficient permissions?")
+        end
+      end
+      tp.set :max_width, max_width
+      tp $users_data
     end
   end
 end
